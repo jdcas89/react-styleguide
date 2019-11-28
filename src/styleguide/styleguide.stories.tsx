15abc52dce1e9890c09d621a99e3a1f4c233c69b
Juan David Castellanos <jd.cast@outlook.com>
2019-11-28 17:03:50 +0100

    done

--- src/styleguide/styleguide.stories.tsx
+++ src/styleguide/styleguide.stories.tsx
@@ -71,7 +71,6 @@ storiesOf('Styleguide ', module)
           </div>
         ))}
       </FlexContainer>
-      <Button>Buy now</Button>
     </Container>
   ));
 
@@ -102,18 +101,4 @@ const Container = styled.div`
   background-color: white;
   height: 100vh;
   padding: 16px;
-`;
-
-const Button = styled.button`
-  background-color: ${({ theme }) => theme.colors.primary};
-  padding: ${({ theme }) => theme.space.M}px;
-  margin: ${({ theme }) => theme.space.M}px;
-  width: 200px;
-  border: none;
-  color: white;
-  font-size: 14px;
-  font-weight: 700;
-  border-radius: 15px;
-  letter-spacing: 2px;
-  text-transform: uppercase;
 `;
